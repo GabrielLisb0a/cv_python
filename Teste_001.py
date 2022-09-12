c9dbc6de1f6276f016a28f2d56ed5cbfd97be519
GabrielLisb0a <113391236+GabrielLisb0a@users.noreply.github.com>
2022-09-12 18:42:21 -0300

    Twste_001

--- Teste_001.py
+++ Teste_001.py
@@ -1,3 +1,3 @@
 real = float(input("Quanto dinheiro você tem na carteira? R$ "))
 dollar = real / 5.17
-print("Com R${:.2f} você pode comprar US${:.2f}".format(real, dollar))
+print(f"Com R${real:.2f} você pode comprar US${dollar:.2f}")
